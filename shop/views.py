082c869ee8a7bbaeadb265a6aef75a007ff998ff
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-10-07 19:04:57 +0530

    Footer and some parts done

--- shop/views.py
+++ shop/views.py
@@ -20,3 +20,15 @@ def collectionsview(request,name):
         messages.warning(request,"No such category found")
         return redirect('collections')
 
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname,status=0)):
+        if(Product.objects.filter(name=pname,status=0)):
+            product = Product.objects.filter(name=pname, status=0).first()
+            return render(request,"shop/products/product_details.html",{"products": products})
+        
+        else:
+            messages.error(request,"No such product found")
+            return redirect('collections')
+    else:
+        messages.error(request,"No such category found")
+        return redirect('collections')
